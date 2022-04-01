1e82c6ac8b73cb47e9c1e4a33c69aa0151561750
Jakub Novak <krthome@gmail.com>
2022-04-01 10:08:29 +0200

    Hotfix funkce rotate()

--- tutorials/files/7/library.py
+++ tutorials/files/7/library.py
@@ -32,3 +32,21 @@ def draw_lines(img, lines):
         cv.line(img_lines, (l[0], l[1]), (l[2], l[3]), (0,0,255), 2, cv.LINE_AA)
     
     return img_lines
+
+def rotate(img, angle):
+    height, width = img.shape[:2]
+    image_center = (width / 2, height / 2)
+
+    rotation_mat = cv.getRotationMatrix2D(image_center, angle, 1.)
+
+    abs_cos = abs(rotation_mat[0, 0])
+    abs_sin = abs(rotation_mat[0, 1])
+
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    rotation_mat[0, 2] += bound_w / 2 - image_center[0]
+    rotation_mat[1, 2] += bound_h / 2 - image_center[1]
+
+    dest = cv.warpAffine(img, rotation_mat, (bound_w, bound_h))
+    return dest
